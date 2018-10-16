3e0540924c7ddcbf72d33ba8e82f2519b8561e73
Agustín Aliaga <agustinaliagac@gmail.com>
2018-10-16 18:26:55 -0300

    Update

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -3,10 +3,10 @@ export default {
         path: `${__PATH_PREFIX__}/`,
         title: 'Blog',
     },
-    projects: {
-        path: '/projects',
-        title: 'Projects',
-    },
+    // projects: {
+    //     path: '/projects',
+    //     title: 'Projects',
+    // },
     aboutMe: {
         path: '/about-me',
         title: 'About Me',
