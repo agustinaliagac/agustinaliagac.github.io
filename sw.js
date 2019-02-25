214948dba221eb74d86d375159533f5af9e466bb
Agustín Aliaga <agustin.aliaga@lavu.com>
2019-02-25 20:00:29 -0300

    Updates

--- sw.js
+++ sw.js
@@ -11,8 +11,8 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("workbox-v3.6.1/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "workbox-v3.6.1"});
+importScripts("workbox-v3.5.0/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.5.0"});
 
 workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
 
@@ -26,40 +26,40 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-455072ca927c28733233.js"
+    "url": "webpack-runtime-1ce37777c6fa055ae4ae.js"
   },
   {
-    "url": "app.7106b9c6fdd909feef5d.css"
+    "url": "app.59193f867c89c229b2e5.css"
   },
   {
-    "url": "app-2c628e97cad5920cf5bd.js"
+    "url": "app-3e472ceb66a42d15a20f.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-4158c07ead1177917d7b.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-0e4b4288b1c39336918e.js"
   },
   {
     "url": "index.html",
-    "revision": "bba39fe3008e4bc74ade8bcb319e8412"
+    "revision": "5e1ff7007b787c39ac4f2ebf60ffaa56"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4276538f894ccf793d7d004b7323cd6a"
+    "revision": "6fce966da893df606d954e1e0d9fce66"
   },
   {
-    "url": "0.3c5fd8dc8e26d3cb63eb.css"
+    "url": "0.6740875e7f9f337d555e.css"
   },
   {
-    "url": "component---src-pages-index-js-f5083531461c725b433e.js"
+    "url": "component---src-pages-index-js-41d6dd6d9e218ced072f.js"
   },
   {
-    "url": "0-cee1a49f5378c1ba6db8.js"
+    "url": "0-4a6c1edcb8f8f2a9758a.js"
   },
   {
     "url": "static/d/349/path---index-6a9-BH2sp8pmt8QeZS3qCtmvXXONltw.json",
     "revision": "5e4c0c1d3cacaf1d805fdbfaaffa65c4"
   },
   {
-    "url": "component---src-pages-404-js-7db2702442c72d08d460.js"
+    "url": "component---src-pages-404-js-238ccf6503f5e8c181d4.js"
   },
   {
     "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
